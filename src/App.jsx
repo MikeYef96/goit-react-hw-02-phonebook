3c5c896e307e4316905252e9d974353bb6d41551
MikeYef96 <mike.yef@gmail.com>
2021-02-24 21:08:03 +0300

    changed type of js files to jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
